69bcee205771bc53e8ce31cecc7194449a52454f
bangnokia <bangnokia@gmail.com>
2024-06-07 03:30:22 +0700

    optimize the deploy process

--- roles/Lunar-CI/files/run.sh
+++ roles/Lunar-CI/files/run.sh
@@ -4,33 +4,48 @@ ls /home | grep -v deploy | grep -Ev "`hostname`|tda|sweb" > /tmp/list_users.txt
 Update_Source()
 {
     filename="/tmp/list_users.txt"
+    deploy_key="/root/.ssh/deploy_rsa.pem"
 
     while IFS= read -r user; do
+        # check if user has the deploy key yet in /home/$user/.ssh/
+        if [ ! -e "$user_deploy_key" ]; then
+            cp "$deploy_key" "/home/$user/.ssh/deploy_rsa.pem"
+            chown $user:$user "/home/$user/.ssh/deploy_rsa.pem"
+            chmod 600 "/home/$user/.ssh/deploy_rsa.pem"
+        fi
+        user_deploy_key="/home/$user/.ssh/deploy_rsa.pem"
+
         for domain_dir in /home/"$user"/domains/*/public_html; do
             if [ -e "$domain_dir/.env" ]; then
                 domain=$(basename $(dirname "$domain_dir"))
                 sudo git config --global --add safe.directory "$domain_dir"
                 cd "$domain_dir" || continue
-                sudo git pull origin main || { notice_fail "$domain"; continue; }
+
+                su - $user -c "GIT_SSH_COMMAND=\"ssh -i $user_deploy_key\" git pull origin main"
+
+                # Check if git pull was successful
+                if [ $? -ne 0 ]; then
+                    notice_fail "$domain"
+                    continue
+                fi
 
                 # we only run when the composer.lock changed
                 if git diff --name-only HEAD@{1} HEAD | grep -qE 'composer\.json|composer\.lock'; then
-                    sudo composer install --no-dev --optimize-autoloader --no-ansi --no-interaction || { notice_fail "$domain"; continue; }
+                    su - $user -c "composer install --no-dev --optimize-autoloader --no-ansi --no-interaction"
                 fi
 
-                # check if
                 if git diff --name-only HEAD@{1} HEAD | grep -qE 'package\.json|pnpm-lock\.yaml|\.js|\.css|\.blade\.php'; then
-                    sudo pnpm install && sudo pnpm run build || { notice_fail "$domain"; continue; }
+                    su - $user -c "pnpm install && pnpm run build"
                 fi
-                sudo php artisan migrate --force || { notice_fail "$domain"; continue; }
-                sudo php artisan optimize || { notice_fail "$domain"; continue; }
-                sudo php artisan icon:cache || { notice_fail "$domain"; continue; }
-                sudo php artisan filament:cache-components || { notice_fail "$domain"; continue; }
-                sudo php artisan deploy:cleanup || { notice_fail "$domain"; continue; }
-                sudo php artisan horizon:terminate || { notice_fail "$domain"; continue; }
-                sudo chown $user:$user -R "$domain_dir"/* || { notice_fail "$domain"; continue; }
-                sudo chown $user:$user -R "$domain_dir"/.* || { notice_fail "$domain"; continue; }
-                sudo systemctl reload php8.2-fpm || { notice_fail "$domain"; continue; }
+                su - $user -c "php artisan migrate --force"
+                su - $user -c "php artisan optimize"
+                su - $user -c "php artisan icon:cache"
+                su - $user -c "php artisan filament:cache-components"
+                su - $user -c "php artisan deploy:cleanup"
+                su - $user -c "php artisan horizon:terminate"
+
+                systemctl reload php8.2-fpm
+
                 echo "Finish build on $domain!" && curl --location 'https://ping2.me/@daudau/sweb-stuff' \
                 --data-urlencode "message=$domain deployed" > /dev/null
             fi
